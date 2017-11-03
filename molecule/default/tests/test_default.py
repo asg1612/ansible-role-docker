608ac3e849e72c1502b759f0318869c91a256bd4
14301253Z <asg1612@gmail.com>
2017-11-03 07:38:03 +0100

    add tests on testinfra

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,16 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_daemon_file(host):
+    f = host.file('/etc/docker/daemon.json')
 
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+def test_docker_service(host):
+    s = host.service('docker')
+
+    assert s.is_enabled
+    assert s.is_running
